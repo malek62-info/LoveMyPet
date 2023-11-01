f46f114c232e51ec951074691709af789e85c583
imane errahmani <imaneerrahmani2@gmail.com>
2023-11-01 17:05:26 +0100

    Created the HomePage

--- src/main/java/com/nanterre/LoveMyPet/Controleur.java
+++ src/main/java/com/nanterre/LoveMyPet/Controleur.java
@@ -11,21 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class Controleur {
 	
 	
-	// pour afficher la page qui permet de faire la calcule
-	    @GetMapping("/Calcule") // cette page on a choisier d'ajouer le /Calculer pour la 
-	    public String showpage() {
-	        return "Calculer";
-	    }
+  @GetMapping("/HomePage")
+  public String homePage() {
+      return "home";
+  }
 	    
-	    // cette methode permer de fair le calculer et elle retourne la page avec le resultat affiché
-	    @PostMapping("/Calcule")// Cette annotation spécifie que cette méthode gère les requêtes POST envoyées à l'URL "/calcule"
-	    // cette annotation on la trouve dans le la commeande Post dans le fichier HTML.
-	    public String calculate(int num1, int num2, Model model) {
-	        int result = num1 + num2;
-	        model.addAttribute("result", num1 + " + " + num2 + " = " + result);
-	        return "Calculer";
-	    }   
-	    
-	}
-
-
+}
