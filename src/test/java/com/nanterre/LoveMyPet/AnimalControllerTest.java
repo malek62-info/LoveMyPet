bf6fc7d52c7b8490376537d15cf2c41cbeb5006c
Malek MESSAOUDI <malekmessaoudi62@gmail.com>
2023-11-24 16:57:29 +0100

    Add test

--- src/test/java/com/nanterre/LoveMyPet/AnimalControllerTest.java
+++ src/test/java/com/nanterre/LoveMyPet/AnimalControllerTest.java
@@ -1,8 +1,8 @@
 
 package com.nanterre.LoveMyPet;
-
 import com.nanterre.LoveMyPet.controller.AnimalController;
 import com.nanterre.LoveMyPet.model.Animal;
+import com.nanterre.LoveMyPet.service.AdoptionService;
 import com.nanterre.LoveMyPet.service.AnimalService;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,12 +13,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class AnimalControllerTest {
 
     @Mock
     private AnimalService animalService;
+
+    @Mock
+    private AdoptionService adoptionService;
 
     @InjectMocks
     private AnimalController animalController;
@@ -29,15 +37,15 @@ public class AnimalControllerTest {
     }
 
     @Test
-    public void testAddAnimal() {
-        // Créez un animal de test avec les données nécessaires
+    public void testAddAnimal() throws Exception {
+        // Créez un objet Animal de test
         Animal animal = new Animal();
 
-        // Créez un fichier image de test
-        MockMultipartFile imageFile = new MockMultipartFile("imageFile", "test-image.jpg", "image/jpeg", "test image content".getBytes());
+        // Créez un fichier bidon pour simuler le téléchargement d'une image
+        MockMultipartFile imageFile = new MockMultipartFile("imageFile", "test.jpg", "image/jpeg", "Spring Framework".getBytes());
 
         // Configurez le comportement attendu lors de l'appel des méthodes du service
-        doNothing().when(animalService).saveAnimal(any(Animal.class));
+        when(animalService.saveAnimal(any(Animal.class))).thenReturn(animal);
 
         // Appelez la méthode à tester
         ResponseEntity<String> responseEntity = animalController.addAnimal(imageFile, animal);
@@ -46,7 +54,33 @@ public class AnimalControllerTest {
         verify(animalService, times(1)).saveAnimal(any(Animal.class));
 
         // Vérifiez la réponse de l'API
-        assert(responseEntity.getStatusCode() == HttpStatus.OK);
-        assert(responseEntity.getBody().equals("Nouvel animal ajouté"));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals("Nouvel animal ajouté", responseEntity.getBody());
     }
+
+
+    @Test
+    public void testAddAnimalWithException() throws Exception {
+        // Créez un objet Animal de test
+        Animal animal = new Animal();
+
+        // Créez un fichier bidon pour simuler le téléchargement d'une image
+        MockMultipartFile imageFile = new MockMultipartFile("imageFile", "test.jpg", "image/jpeg", "Spring Framework".getBytes());
+
+        // Configurez le comportement attendu lors de l'appel des méthodes du service
+        doThrow(new RuntimeException("Erreur lors de l'ajout de l'animal")).when(animalService).saveAnimal(any(Animal.class));
+
+        // Appelez la méthode à tester
+        ResponseEntity<String> responseEntity = animalController.addAnimal(imageFile, animal);
+
+        // Vérifiez que la méthode du service a été appelée une fois
+        verify(animalService, times(1)).saveAnimal(any(Animal.class));
+
+        // Vérifiez la réponse de l'API
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        assertEquals("Erreur lors de la gestion de l'image", responseEntity.getBody());
+    }
+
+    // Les autres tests pour addAdoption que nous avons déjà discutés...
+
 }
--- src/test/java/com/nanterre/LoveMyPet/PersonControllerTest.java
+++ src/test/java/com/nanterre/LoveMyPet/PersonControllerTest.java
@@ -2,7 +2,9 @@ package com.nanterre.LoveMyPet;
 
 import com.nanterre.LoveMyPet.controller.PersonController;
 import com.nanterre.LoveMyPet.model.Person;
+import com.nanterre.LoveMyPet.model.Candidature;
 import com.nanterre.LoveMyPet.service.PersonService;
+import com.nanterre.LoveMyPet.service.CandidatureService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -11,7 +13,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.mock.web.MockMultipartFile;
 
 import java.util.Map;
 
@@ -22,6 +23,9 @@ public class PersonControllerTest {
 
     @Mock
     private PersonService personService;
+
+    @Mock
+    private CandidatureService candidatureService;
 
     @InjectMocks
     private PersonController personController;
@@ -36,12 +40,10 @@ public class PersonControllerTest {
         Person person = new Person();
         person.setEmail("test@example.com");
 
-        MockMultipartFile imageFile = new MockMultipartFile("imageFile", "test-image.jpg", "image/jpeg", "test image content".getBytes());
-
         when(personService.findPersonByEmail("test@example.com")).thenReturn(null);
         doNothing().when(personService).savePerson(any(Person.class));
 
-        ResponseEntity<String> responseEntity = personController.add(imageFile, person);
+        ResponseEntity<String> responseEntity = personController.add(null, person);
 
         verify(personService, times(1)).savePerson(any(Person.class));
 
@@ -50,19 +52,16 @@ public class PersonControllerTest {
     }
 
     @Test
-    public void testLogin() {
-        MockHttpSession session = new MockHttpSession();
+    public void testLoginSuccess() {
+        Person user = new Person();
+        user.setEmail("test@example.com");
+        user.setPassword("password123");
 
-        Person person = new Person();
-        person.setEmail("test@example.com");
-        person.setPassword("password123");
+        when(personService.findPersonByEmail("test@example.com")).thenReturn(user);
 
-        when(personService.findPersonByEmail("test@example.com")).thenReturn(person);
+        ResponseEntity<?> responseEntity = personController.login("test@example.com", "password123", null);
 
-        ResponseEntity<?> responseEntity = personController.login("test@example.com", "password123", session);
-
-        verify(session, times(1)).setAttribute("userId", person.getIdPerson());
-        verify(session, times(1)).setAttribute("email", "test@example.com");
+        verify(personService, never()).findPersonByEmail("test@example.com");
 
         assert(responseEntity.getStatusCode() == HttpStatus.OK);
         assert(responseEntity.getBody().equals("Authentification réussie"));
@@ -89,4 +88,29 @@ public class PersonControllerTest {
         assert(userData.get("id").equals(person.getIdPerson()));
         assert(userData.get("name").equals("John Doe"));
     }
+
+    @Test
+    public void testAddCandidature() {
+        doNothing().when(candidatureService).saveCandidature(any(Candidature.class));
+
+        ResponseEntity<String> responseEntity = personController.addCandidature(1, 2, "2023-11-24");
+
+        verify(candidatureService, times(1)).saveCandidature(any(Candidature.class));
+
+        assert(responseEntity.getStatusCode() == HttpStatus.OK);
+        assert(responseEntity.getBody().equals("Candidature ajoutée avec succès"));
+    }
+
+    @Test
+    public void testAddCandidatureFailure() {
+        doThrow(new RuntimeException("Erreur lors de l'ajout de la candidature")).when(candidatureService).saveCandidature(any(Candidature.class));
+
+        ResponseEntity<String> responseEntity = personController.addCandidature(1, 2, "2023-11-24");
+
+        verify(candidatureService, times(1)).saveCandidature(any(Candidature.class));
+
+        assert(responseEntity.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR);
+        assert(responseEntity.getBody().equals("Erreur lors de l'ajout de la candidature"));
+    }
 }
+
