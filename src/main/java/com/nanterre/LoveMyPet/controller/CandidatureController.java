2c14513a697e7bee8a4ae3d790ef588febda8e19
faiz adenle <adenle.sadikoufaiz@gmail.com>
2023-11-25 18:46:42 +0100

    Resolve merge problem

--- src/main/java/com/nanterre/LoveMyPet/controller/CandidatureController.java
+++ src/main/java/com/nanterre/LoveMyPet/controller/CandidatureController.java
@@ -10,7 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/animal/{animalId}/candidature")
 public class CandidatureController {
-
     @Autowired
     private CandidatureServiceImpl candidatureService;
 
