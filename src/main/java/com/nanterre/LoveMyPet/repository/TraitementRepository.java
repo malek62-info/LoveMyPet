28636ad91895d70c9559184a0d74d02e72a79f07
faiz adenle <adenle.sadikoufaiz@gmail.com>
2024-03-02 14:52:02 +0100

    Add notifications for treatment

--- src/main/java/com/nanterre/LoveMyPet/repository/TraitementRepository.java
+++ src/main/java/com/nanterre/LoveMyPet/repository/TraitementRepository.java
@@ -1,7 +1,6 @@
 package com.nanterre.LoveMyPet.repository;
 
 import com.nanterre.LoveMyPet.model.Traitement;
-import com.nanterre.LoveMyPet.model.Vaccination;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +12,27 @@ import java.util.Optional;
 public interface TraitementRepository extends JpaRepository<Traitement, Integer> {
     List<Traitement> findByAnimalId(Integer animalId);
     Optional<Traitement> findById(Integer id);
+
+    //Liste des details traitement actuel
+    @Query(nativeQuery = true, value = "SELECT person.email, " +
+            "medicament.medicamentname AS nom_medicament, " +
+            "traitement.commentaire AS nom_traitement, " +
+            "DATE_FORMAT(CURDATE(), '%Y-%m-%d') AS date_traitement, " +
+            "animal.name AS nom_animal, " +
+            "DATE_FORMAT(heure.heure, '%H:%i') AS heure_traitement " +
+            "FROM traitement " +
+            "JOIN animal ON traitement.idanimal = animal.idanimal " +
+            "JOIN person ON animal.idperson = person.id_person " +
+            "LEFT JOIN medicament ON traitement.idmedicament = medicament.idmedicament " +
+            "LEFT JOIN heure ON traitement.idtraitement = heure.idtraitement " +
+            "WHERE CURRENT_DATE >= traitement.datedebut " +
+            "AND CURRENT_DATE <= traitement.datefin " +
+            "AND heure.heure IS NOT NULL " +
+            "AND HOUR(CURRENT_TIME()) = HOUR(heure.heure) " +
+            "AND MINUTE(CURRENT_TIME()) = MINUTE(heure.heure)")
+    List<Object[]> findEmailAndTreatmentDetails();
+
+
+
+
 }
---
+++ src/main/java/com/nanterre/LoveMyPet/service/ScheduledTraitementService.java
@@ -0,0 +1,89 @@
+package com.nanterre.LoveMyPet.service;
+
+import com.nanterre.LoveMyPet.repository.TraitementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ScheduledTraitementService {
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @Autowired
+    private TraitementRepository traitementRepository;
+
+    // Cette fonction envoie des e-mails  pour les traitements
+    @Scheduled(fixedRate = 60000)
+    public void sendEmailToUsersWithScheduledTraitement() {
+        try {
+            List<Object[]> traitementDetails = traitementRepository.findEmailAndTreatmentDetails();
+
+          /*  for (Object[] traitementDetail : traitementDetails) {
+                System.out.println(Arrays.toString(traitementDetail));
+            }*/
+
+            for (Object[] traitementDetail : traitementDetails) {
+                if (traitementDetail != null && traitementDetail.length == 6) {
+
+                    String userEmail = (String) traitementDetail[0];         // email de la personne
+                    String nom_medicament = (String) traitementDetail[1];   // nom du médicament
+                    String nom_traitement = (String) traitementDetail[2];    // nom du traitement
+                    String date_traitement = (String) traitementDetail[3];   // date du traitement
+                    String nom_animal = (String) traitementDetail[4];        // nom de l'animal
+                    String heure_traitement = (String) traitementDetail[5];  // heure du traitement
+
+
+                /*    System.out.println("Email: " + userEmail);
+                    System.out.println("Médicament: " + nom_medicament);
+                    System.out.println("Traitement: " + nom_traitement);
+                    System.out.println("Date du traitement: " + date_traitement);
+                    System.out.println("Animal: " + nom_animal);
+                    System.out.println("Heure du traitement: " + heure_traitement);*/
+
+
+                    String subject = "LoveMyPet Rappel - Traitement pour " + nom_animal ;
+
+                    // Corps de l'e-mail avec HTML et CSS
+                    String body = "<html><head>"
+                            + "<style>"
+                            + "body {font-family: Arial, sans-serif;}"
+                            + ".container {max-width: 600px; margin: 0 auto;}"
+                            + ".header {background-color: #000; color: white; padding: 20px; text-align: center;}"
+                            + ".content {padding: 20px;}"
+                            + ".content p {margin-bottom: 10px; color: #000;}"
+                            + ".content p span {font-weight: bold;}"
+                            + ".footer {background-color: #f4f4f4; padding: 20px; text-align: center;}"
+                            + "</style>"
+                            + "</head><body>"
+                            + "<div class='container'>"
+                            + "<div class='header'><h2>LoveMyPet Rappel</h2></div>"
+                            + "<div class='content'>"
+                            + "<p>Bonjour,</p>"
+                            + "<p>Il est maintenant temps de donner le traitement à <span>" + nom_animal + "</span>.</p>"
+                            + "<p><span>Date du traitement :</span> " + date_traitement + "</p>"
+                            + "<p><span>Heure du traitement :</span> " + heure_traitement + "</p>"
+                            + "<p><span>Médicament :</span> " + nom_medicament + "</p>"
+                            + "<p><span>Nom du traitement :</span> " + nom_traitement + "</p>"
+                            + "<p>N'oubliez pas de donner le traitement à votre animal à l'heure prévue.</p>"
+                            + "<p>Merci de prendre soin de votre animal!</p>"
+                            + "<div style=\"text-align: center; margin-top: 20px;\"><a href=\"https://imgbb.com/\"><img src=\"https://i.ibb.co/YWY5Tbf/s-l1200.webp\" alt=\"vaccination-chien\" width=\"200px\" style=\"border-radius: 10px; display: block; margin: 0 auto;\"></a></div>"
+                            + "</div><div class='footer'>Cordialement<br/>LoveMyPet</div></div>"
+                            + "</body></html>";
+
+
+
+                    emailSenderService.sendHtmlEmail(userEmail, subject, body);
+
+                    System.out.println("Email sent successfully to " + userEmail + " (Traitement Email)");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error sending emails: " + e.getMessage());
+        }
+    }
+}
