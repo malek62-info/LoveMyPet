4e78b92a6109eaea9d5f4b7f610320074256e1fd
faiz adenle <adenle.sadikoufaiz@gmail.com>
2024-04-28 20:04:15 +0200

    Graphic test for login

---
+++ graphics-tests/Testlogin.py
@@ -0,0 +1,36 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+driver_path = 'msedgedriver.exe'
+edge_options = webdriver.EdgeOptions()
+edge_options.use_chromium = True
+
+driver = webdriver.Edge(options=edge_options)
+driver.get('http://localhost:8086/login')
+time.sleep(2)
+
+# Remplir le champ Email
+email_field = driver.find_element("id", 'email')
+email_field.send_keys('faiz@gmail.com')
+
+
+password_field = driver.find_element("id", 'password')
+password_field.send_keys('faiz')
+
+
+login_button = driver.find_element("name", 'submit')
+login_button.click()
+
+try:
+    WebDriverWait(driver, 10).until(
+        EC.url_to_be("http://localhost:8086/profile")
+    )
+    print("Connexion réussie ! Vous êtes sur la page de profil.")
+   
+except TimeoutException:
+    print("La page de profil n'a pas été chargée dans les délais.")
